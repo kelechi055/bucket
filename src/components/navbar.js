2d2f216be083f918b52c264e6721a3d7040893ac
Kelechi Opurum <157651060+kelechi055@users.noreply.github.com>
2025-04-06 07:37:07 -0400

    📊 updated nav bar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,7 +8,10 @@ export default function Navbar() {
 
   return (
     <AppBar position="fixed" sx={{ backgroundColor: 'black' }}>
-      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+      <Toolbar
+        className="font-fredoka"
+        sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}
+      >
         {/* Left side (Bucket Logo) */}
         <Box
           sx={{
@@ -55,14 +58,17 @@ export default function Navbar() {
               {item.label}
             </Button>
           ))}
-          {/* Display SignIn/SignUp buttons if not signed in */}
           {!isSignedIn ? (
             <>
               <SignInButton mode="modal">
-                <button className="border px-4 py-2 rounded border-white">Sign In</button>
+                <button className="border px-4 py-2 rounded border-white text-white hover:bg-white hover:text-black transition-all">
+                  Sign In
+                </button>
               </SignInButton>
               <SignUpButton mode="modal">
-                <button className="bg-white text-black px-4 py-2 rounded">Sign Up</button>
+                <button className="bg-white text-black px-4 py-2 rounded hover:bg-gray-200 transition-all">
+                  Sign Up
+                </button>
               </SignUpButton>
             </>
           ) : (
